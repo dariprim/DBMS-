251e8df54082fc255393a2ab60872342b7cbc612
dariprim <dasha.primak.2014@mail.ru>
2026-02-10 00:30:50 +0300

    database

---
+++ lab1/SQLQuery_1.sql
@@ -0,0 +1,49 @@
+-- CREATE database Apartment;
+
+GO
+USE Apartment;
+GO
+
+-- CREATE TABLE Privatization (
+--     Address NVARCHAR(60),
+--     District NVARCHAR(15),
+--     Balance NVARCHAR(60),
+--     Year INT,
+--     Material NVARCHAR(15),
+--     Base NVARCHAR(15),
+--     Wear INT,
+--     Flow INT,
+--     Line INT,
+--     Square INT,
+--     Hall BIT,
+--     Flats INT,
+--     Elevator BIT,
+--     Flat INT,
+--     Storey INT,
+--     Rooms INT,
+--     SquareFlat INT,
+--     Dwell INT,
+--     Branch INT,
+--     Balcony INT,
+--     Height INT,
+--     Record INT,
+--     Document NVARCHAR(60),
+--     DateDoc DATE,
+--     Cost MONEY,
+--     FioHost NVARCHAR(60),
+--     Pasport NVARCHAR(60),
+--     Sign BIT,
+--     Born INT,
+--     Status NVARCHAR(20)
+-- );
+-- GO
+
+-- INSERT INTO Privatization VALUES
+-- (N'ул. Ленина, 10', N'Центральный', N'ГорЖилФонд', 1985, N'Кирпич', N'Бетон', 30, 5, 500, 5000, 1, 50, 1, 25, 3, 2, 60, 40, 10, 5, 3, 1, N'Договор №123', '2023-01-15', 1500000, N'Иванов И.И.', N'4500 123456', 1, 1970, N'Собственник'),
+-- (N'ул. Мира, 5', N'Северный', N'ГорЖилФонд', 1990, N'Панель', N'Бетон', 20, 9, 3000, 7000, 0, 100, 1, 45, 5, 3, 75, 50, 15, 8, 3, 2, N'Договор №124', '2023-02-20', 2000000, N'Петров П.П.', N'4501 654321', 1, 1980, N'Собственник'),
+-- (N'ул. Зеленая, 15', N'Западный', N'Частный', 2000, N'Кирпич', N'Бетон', 10, 3, 4000, 3000, 1, 20, 0, 10, 1, 1, 40, 30, 5, 3, 3, 3, N'Договор №125', '2023-03-10', 1000000, N'Сидоров С.С.', N'4502 789012', 0, 1990, N'Арендатор'),
+-- (N'ул. Лесная, 8', N'Восточный', N'ГорЖилФонд', 1975, N'Панель', N'Бетон', 40, 4, 2000, 4000, 0, 40, 0, 12, 2, 2, 55, 35, 10, 4, 3, 4, N'Договор №126', '2023-04-05', 1200000, N'Кузнецов К.К.', N'4503 345678', 1, 1965, N'Собственник');
+-- GO
+
+-- SELECT * FROM Privatization;
+
